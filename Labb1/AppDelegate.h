dc62817b0fb8020d8a05c5479971372b47312a0f
ITHS <ITHS@ithss-air.lan>
2018-01-16 19:39:08 +0100

    Initial Commit

---
+++ Labb1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Labb1
+//
+//  Created by ITHS on 2018-01-16.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Labb1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Labb1
+//
+//  Created by ITHS on 2018-01-16.
+//  Copyright © 2018 MoaLenngren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
